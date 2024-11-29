5340c2edff4fe1c0b567346ff64a7036ca2fd1e2
Toandro <toandro@Toandros-MacBook-Pro.local>
2024-11-29 17:31:27 +0300

    version complete de migration

---
+++ prisma/migrations/20241129143100_init/migration.sql
@@ -0,0 +1,294 @@
+-- CreateTable
+CREATE TABLE "Categories" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "description" TEXT,
+    "parent_category_id" INTEGER,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Companies" (
+    "id" SERIAL NOT NULL,
+    "company_name" VARCHAR NOT NULL,
+    "NIF" VARCHAR NOT NULL,
+    "STAT" VARCHAR NOT NULL,
+    "localisation" VARCHAR,
+    "created_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Company_members" (
+    "user_id" INTEGER NOT NULL,
+    "company_id" INTEGER NOT NULL,
+    "role" VARCHAR DEFAULT 'member',
+    "joined_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "Company_members_pkey" PRIMARY KEY ("user_id","company_id")
+);
+
+-- CreateTable
+CREATE TABLE "Conversations" (
+    "id" SERIAL NOT NULL,
+    "user1_id" INTEGER NOT NULL,
+    "user2_id" INTEGER NOT NULL,
+    "last_message_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "conversations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Followers" (
+    "id" SERIAL NOT NULL,
+    "follower_id" INTEGER NOT NULL,
+    "following_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "followers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Messages" (
+    "id" SERIAL NOT NULL,
+    "sender_id" INTEGER NOT NULL,
+    "receiver_id" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "is_read" BOOLEAN DEFAULT false,
+    "created_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+    "conversation_id" INTEGER NOT NULL,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notifications" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "type" VARCHAR NOT NULL,
+    "reference_id" INTEGER,
+    "is_read" BOOLEAN DEFAULT false,
+    "created_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+    "sub_type" VARCHAR,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Publication_comments" (
+    "id" SERIAL NOT NULL,
+    "publication_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "comment_text" TEXT NOT NULL,
+    "created_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "publication_comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Publications" (
+    "id" SERIAL NOT NULL,
+    "owner_user_id" INTEGER,
+    "owner_company_id" INTEGER,
+    "publication_type" VARCHAR NOT NULL,
+    "publication_title" VARCHAR NOT NULL,
+    "content" TEXT NOT NULL,
+    "media" VARCHAR,
+    "category_id" INTEGER,
+    "price" DECIMAL(10,2),
+    "initial_stock" DECIMAL(10,2),
+    "current_stock" DECIMAL(10,2),
+    "max_weight" DECIMAL(10,2),
+    "min_weight" DECIMAL(10,2),
+    "devise_type" VARCHAR DEFAULT 'mga',
+    "livraison" BOOLEAN DEFAULT false,
+    "location" VARCHAR NOT NULL,
+    "status" VARCHAR DEFAULT 'active',
+    "created_at" TIMESTAMP(6),
+    "updated_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "publications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reactions" (
+    "id" SERIAL NOT NULL,
+    "publication_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "reaction_type" VARCHAR NOT NULL DEFAULT 'like',
+    "created_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "reactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reviews" (
+    "id" SERIAL NOT NULL,
+    "transaction_id" INTEGER NOT NULL,
+    "reviewer_id" INTEGER NOT NULL,
+    "reviewee_id" INTEGER NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "created_at" TIMESTAMP(6),
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transactions" (
+    "id" SERIAL NOT NULL,
+    "buyer_id" INTEGER NOT NULL,
+    "seller_id" INTEGER NOT NULL,
+    "publication_id" INTEGER NOT NULL,
+    "quantity" INTEGER DEFAULT 1,
+    "total_price" DECIMAL(10,2) NOT NULL,
+    "transaction_date" TIMESTAMP(6) NOT NULL,
+    "status" VARCHAR DEFAULT 'pending',
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL,
+    "password" VARCHAR NOT NULL,
+    "created_at" TIMESTAMP(6),
+    "country" VARCHAR,
+    "city" VARCHAR,
+    "company" BOOLEAN DEFAULT false,
+    "language" VARCHAR,
+    "profile_picture" VARCHAR,
+    "is_deleted" BOOLEAN DEFAULT false,
+    "is_premium_user" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" SERIAL NOT NULL,
+    "buyer_id" INTEGER NOT NULL,
+    "seller_id" INTEGER NOT NULL,
+    "publication_id" INTEGER NOT NULL,
+    "quantity" INTEGER DEFAULT 1,
+    "total_price" DECIMAL(10,2) NOT NULL,
+    "transaction_date" TIMESTAMP(6) NOT NULL,
+    "status" VARCHAR DEFAULT 'pending',
+    "is_deleted" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "Categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_company_name_key" ON "Companies"("company_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_NIF_key" ON "Companies"("NIF");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "companies_STAT_key" ON "Companies"("STAT");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "Users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "Users"("email");
+
+-- AddForeignKey
+ALTER TABLE "Categories" ADD CONSTRAINT "categories_parent_category_id_fkey" FOREIGN KEY ("parent_category_id") REFERENCES "Categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Company_members" ADD CONSTRAINT "company_members_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "Companies"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Company_members" ADD CONSTRAINT "company_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Conversations" ADD CONSTRAINT "conversations_user1_id_fkey" FOREIGN KEY ("user1_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Conversations" ADD CONSTRAINT "conversations_user2_id_fkey" FOREIGN KEY ("user2_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Followers" ADD CONSTRAINT "followers_follower_id_fkey" FOREIGN KEY ("follower_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Followers" ADD CONSTRAINT "followers_following_id_fkey" FOREIGN KEY ("following_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Messages" ADD CONSTRAINT "fk_conversation" FOREIGN KEY ("conversation_id") REFERENCES "Conversations"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Messages" ADD CONSTRAINT "messages_receiver_id_fkey" FOREIGN KEY ("receiver_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Messages" ADD CONSTRAINT "messages_sender_id_fkey" FOREIGN KEY ("sender_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Notifications" ADD CONSTRAINT "notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Publication_comments" ADD CONSTRAINT "publication_comments_publication_id_fkey" FOREIGN KEY ("publication_id") REFERENCES "Publications"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Publication_comments" ADD CONSTRAINT "publication_comments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Publications" ADD CONSTRAINT "publications_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Publications" ADD CONSTRAINT "publications_owner_company_id_fkey" FOREIGN KEY ("owner_company_id") REFERENCES "Companies"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Publications" ADD CONSTRAINT "publications_owner_user_id_fkey" FOREIGN KEY ("owner_user_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Reactions" ADD CONSTRAINT "reactions_publication_id_fkey" FOREIGN KEY ("publication_id") REFERENCES "Publications"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Reactions" ADD CONSTRAINT "reactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Reviews" ADD CONSTRAINT "reviews_reviewee_id_fkey" FOREIGN KEY ("reviewee_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Reviews" ADD CONSTRAINT "reviews_reviewer_id_fkey" FOREIGN KEY ("reviewer_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Reviews" ADD CONSTRAINT "reviews_transaction_id_fkey" FOREIGN KEY ("transaction_id") REFERENCES "Transactions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Transactions" ADD CONSTRAINT "transactions_buyer_id_fkey" FOREIGN KEY ("buyer_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Transactions" ADD CONSTRAINT "transactions_publication_id_fkey" FOREIGN KEY ("publication_id") REFERENCES "Publications"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Transactions" ADD CONSTRAINT "transactions_seller_id_fkey" FOREIGN KEY ("seller_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_buyer_id_fkey" FOREIGN KEY ("buyer_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_publication_id_fkey" FOREIGN KEY ("publication_id") REFERENCES "Publications"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_seller_id_fkey" FOREIGN KEY ("seller_id") REFERENCES "Users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
